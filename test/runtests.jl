ff3f3c55f95492ebf162809095c679ad4d05f45d
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-06-24 16:26:50 -0500

    Disable ctors test on Julia < 1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,9 @@ using SpikingNeuralNetworks
 include(joinpath(@__DIR__, "..", "src", "units.jl")) # FIXME
 using Test
 
+if VERSION < v"1.1"
 include("ctors.jl")
+end
 include("chain.jl")
 include("hh_net.jl")
 include("hh_neuron.jl")

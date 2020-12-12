a2c2863ce10ee3b811aac76207423467ad4d2823
Russell Jarvis <russelljarvis@protonmail.com>
2020-12-12 14:52:37 +1100

    adexp integrate and fire neuron

--- src/neuron/adexp.jl
+++ src/neuron/adexp.jl
@@ -16,13 +16,8 @@ Romain Brette, Ecole Normale Supérieure, Paris, France
     v_reset::FT = -70.6
     spike_delta::FT = 30
 end
-
-
-
-#@snn_kw mutable struct IZ{VFT=Vector{Float32},VBT=Vector{Bool}}
 @snn_kw mutable struct AD{VFT=Vector{Float32},VBT=Vector{Bool}}
-    param::ADEXParameter = ADEXParameter()
-    #=a,
+    param::ADEXParameter = ADEXParameter(a,
                                         b,
                                         cm,
                                         v_rest,
@@ -32,41 +27,40 @@ end
                                         delta_T,
                                         v_spike,
                                         v_reset,
-                                        spike_delta)=#
+                                        spike_delta)
     N::Int32 = 1
-    cnt::Int32 = 2
+    cnt::Int32 = 1
     v::VFT = fill(param.v_rest, N)
     w::VFT = zeros(N)
     fire::VBT = zeros(Bool, N)
     I::VFT = zeros(N)
+    sized::Int32 = 1
     spike_raster::Vector{Int32} = zeros(N)
     records::Dict = Dict()
 end
 
 function integrate!(p::AD, param::ADEXParameter, dt::Float32)
-    @unpack N, cnt, v, w, fire, I,spike_raster = p
+    @unpack N, cnt, v, w, fire, I,spike_raster,sized = p
     @unpack a,b,cm,v_rest,tau_m,tau_w,v_thresh,delta_T,v_spike,v_reset,spike_delta = param
-    if spike_raster[cnt-1] == 1 || fire[1]
-      v[1] = v_reset
-      w[1] += b
+    @inbounds for i = 1:N
+        if spike_raster[cnt] == 1 || fire[i]
+          v[i] = v_reset
+          w[i] += b
+        end
+        dv  = (((v_rest-v[i]) +
+                delta_T*exp((v[i] - v_thresh)/delta_T))/tau_m +
+                (I[i] - w[i])/cm) *dt
+        v[i] += dv
+        w[i] += dt * (a*(v[i] - v_rest) - w[i])/tau_w * dt
+        fire[i] = v[i] > v_thresh
+        if v[i]>v_thresh
+            fire[i] = 1
+            v[i] = spike_delta
+            spike_raster[cnt] = 1
+
+        else
+            spike_raster[cnt] = 0
+        end
     end
-    dv  = (((v_rest-v[1]) +
-            delta_T*exp((v[1] - v_thresh)/delta_T))/tau_m +
-            (I[1] - w[1])/cm) *dt
-    v[1] += dv
-    w[1] += dt * (a*(v[1] - v_rest) - w[1])/tau_w * dt
-
-
-    fire[1] = v[1] > v_thresh
-
-    if v[1]>v_thresh
-        fire[1] = 1 # v[1] > vPeak
-        v[1] = spike_delta
-        spike_raster[cnt] = 1
-
-    else
-        spike_raster[cnt] = 0
-    end
-
     cnt+=1
 end
